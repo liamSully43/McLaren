e42707c1a28716d6ad7b7fb6fa381ad576c08e05
Liam Sullivan <60324016+liamSully43@users.noreply.github.com>
2023-08-21 22:27:21 +0100

    added parallax scroll to images and linked logos up to hero section

--- app.js
+++ app.js
@@ -11,16 +11,38 @@ burger_menu_button.onclick = function() {
 }
 
 // === Track Scoll Position ===
-const logo = document.querySelector("#logo");
-
 const body = document.querySelector("body");
 body.addEventListener("scroll", function(event) {
     const scroll = event.target.scrollTop;
     logo_position(scroll);
+    image_positioning(scroll, 2100, 0); // how it started
+    image_positioning(scroll, 4200, 1); // the first win
+    image_positioning(scroll, 12000, 2); // current drivers
 });
 
 // === Adjust Logo Position ===
+const logo = document.querySelector("#logo");
 function logo_position(scroll) {
     const scroll_pos = scroll + 40 - 305; // scroll + margin-left - nav width 
     logo.style.setProperty("--scroll", `${scroll_pos}px`);
+}
+
+// === Adjust Image Position ===
+const sections = document.querySelectorAll(".parallax-images");
+function image_positioning(scroll, start_point, section_index) {
+    // calculate how much the images should parallax scroll
+    const diff = scroll - start_point;
+    const pos1 = diff * 0.2;
+    const pos2 = diff * 0.01;
+
+    // apply the parallax scroll to the images
+    const imgs = sections[section_index].querySelectorAll("img");
+    // translateX(0px)
+    imgs[0].style.transform = `translateX(${0 - pos1}px)`;
+    imgs[1].style.transform = `translateX(${0 - pos2}px)`;
+
+    // only applies to 'current drivers' section
+    if(imgs[2]) {
+        imgs[2].style.transform = `translateX(${0 - pos1}px)`;
+    }
 }
